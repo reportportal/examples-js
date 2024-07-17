64f5daceb85e4ecc707717b470a0e3da27453e52
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2024-07-17 16:51:33 +0200

    Postman example. Actualize deps

--- example-postman/index.js
+++ example-postman/index.js
@@ -7,10 +7,10 @@ newman.run(
     reporter: {
       '@reportportal/agent-js-postman': {
         endpoint: 'http://your-instance.com:8080/api/v1',
-        token: '00000000-0000-0000-0000-000000000000',
-        launch: 'LAUNCH_NAME',
-        project: 'PROJECT_NAME',
-        description: 'PROJECT_DESCRIPTION',
+        apiKey: '<API_KEY>',
+        launch: 'Your launch name',
+        project: 'Your reportportal project name',
+        description: 'Your launch description',
         attributes: [
           {
             key: 'attributeKey',
@@ -20,7 +20,6 @@ newman.run(
             value: 'secondAttrbuteValue',
           },
         ],
-        debug: true,
       },
     },
   },
