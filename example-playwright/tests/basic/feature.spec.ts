a889c500dd115454eb3d615e8ea8cf09e7ad2f9d
Ilya_Hancharyk <Ilya_Hancharyk@epam.com>
2025-10-15 14:21:44 +0300

    Playwright example. Remove redundant line

--- example-playwright/tests/basic/feature.spec.ts
+++ example-playwright/tests/basic/feature.spec.ts
@@ -95,7 +95,6 @@ test('Test to pass', async ({ page, browserName }) => {
       value: 'low',
     },
   ]);
-  await page
   await page.goto('https://playwright.dev/');
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
